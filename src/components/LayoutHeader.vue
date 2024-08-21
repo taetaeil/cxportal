1b3e40b2cdd33ee1daad1e5bf13f2a8895fc7e92
hanzsver <hanzsver@gmail.com>
2024-08-21 15:57:11 +0900

    fix: header 데이터 바인딩

--- src/components/LayoutHeader.vue
+++ src/components/LayoutHeader.vue
@@ -1,4 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const noticeCont = ref(
+  '공지사항이 여기에 전광판처럼 보여집니다.공지사항이 여기에 전광판처럼 보여집니다.공지사항이 여기에 전광판처럼 보여집니다.'
+)
+
+const userInfo = reactive({
+  userNm: '홍길동',
+  accessDate: '0000/00/00',
+  accessTime: '00:00:00',
+  teamNm: 'Consumer 부문'
+})
+
+const menuItems = reactive([
+  { menuNum: 0, name: '문의콜', isActive: true },
+  { menuNum: 1, name: '불만콜', isActive: false },
+  { menuNum: 2, name: '통합 NPS', isActive: false },
+  { menuNum: 3, name: 'J-NPS', isActive: false },
+  { menuNum: 4, name: '고객의 소리', isActive: false }
+])
+
+const currentMenu = ref(0)
+
+const setActive = (i: number) => {
+  currentMenu.value = i
+}
+</script>
 
 <template>
   <header class="header">
@@ -12,11 +37,10 @@
             </a>
           </h1>
           <div class="main-header__board">
-            <Icon name="loud-speaker__line--999" width="24" height="24" alt="" aria-hidden="true" />
+            <Icon name="loud-speaker__line--999" width="24" height="24" alt="공지사항" />
             <div class="main-header__board-wrap">
               <div class="main-header__board-content">
-                공지사항이 여기에 전광판처럼 보여집니다.공지사항이 여기에 전광판처럼
-                보여집니다.공지사항이 여기에 전광판처럼 보여집니다.
+                {{ noticeCont }}
               </div>
             </div>
           </div>
@@ -28,14 +52,14 @@
           <div class="main-header__user">
             <div class="main-header__user-wrap">
               <div class="main-header__user-detail">
-                <em>홍길동</em>
-                <span>0000/00/00</span>
-                <span>00:00:00</span>
+                <em>{{ userInfo.userNm }}</em>
+                <span>{{ userInfo.accessDate }}</span>
+                <span>{{ userInfo.accessTime }}</span>
                 <button type="button">
                   <Icon name="arrow-d__line--333" width="24" height="24" alt="펼치기" />
                 </button>
               </div>
-              <span> Consumer 부분 </span>
+              <span> {{ userInfo.teamNm }} </span>
             </div>
           </div>
         </div>
@@ -43,10 +67,15 @@
     </div>
     <!-- GNB -->
     <nav class="gnb">
-      <ul>
-        <li class="is-active"><a href="">문의콜</a></li>
-        <li><a href="">불만콜</a></li>
-        <li><a href="">고객의 소리</a></li>
+      <ul class="gnb__inner">
+        <li
+          v-for="menu in menuItems"
+          :key="`gnb-menu-${menu.menuNum}`"
+          :class="{ 'is-active': menu.menuNum === currentMenu }"
+          @click="setActive(menu.menuNum)"
+        >
+          <a href="#">{{ menu.name }}</a>
+        </li>
       </ul>
     </nav>
   </header>
