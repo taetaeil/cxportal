9aec738d2b19a2e2b6c2d4959b1079ae1e8bf1db
taetaeil <arlaxodlfa@gmail.com>
2024-08-21 13:54:04 +0900

    Feat(customInput): 개발자 사전 작업 소스 추가
    Feat(input): Basic / Search 퍼블리싱
    
    Refactor(icon): 미사용 아이콘 파일 임시 주석 처리

--- src/examples/pages/input.vue
+++ src/examples/pages/input.vue
@@ -17,40 +17,53 @@ const form = {
     <h2 class="text-3xl font-semibold">
       Input
     </h2>
-    <div class="flex flex-col gap-2 mt-10 p-10 bg-white rounded-3xl">
+    <!-- <div class="flex flex-col gap-2 mt-10 p-10 bg-white rounded-3xl">
       <p><em class="font-bold">use-col-group</em>은 <em class="font-bold">:list</em> 사용이 가능합니다. 아래 예제는 list를 사용했습니다.</p>
       <p><em class="font-bold">form-col</em> 일때만 <em class="font-bold">use-btn</em>사용이 가능합니다.</p>
       <p class="text-rose-500">&#8251; 아래 예제 중 <em class="font-bold">타겟 테이블의 형태</em>는 <em
           class="font-bold">form-col</em>로만
         사용합니다.</p>
-    </div>
+    </div> -->
     <!-- input -->
     <div class="my-10">
-      <form class="form">
-        <FormItem label="Basic">
-          <CustomInput v-model="noData" max-length="10" placeholder="10글자 내로 입력하세요." />
+      <mark class="inline-block text-xl font-bold" style="background:#F0E4FF">
+        Basic
+      </mark>
+      <form class="form content-box">
+        <FormItem label="Default">
+          <CustomInput v-model="noData" placeholder="내용을 입력하세요." />
         </FormItem>
-        <FormItem label="size=&quot;sm&quot;">
-          <CustomInput v-model="form.input" size="sm" />
-        </FormItem>
-        <FormItem label="size=&quot;lg&quot;">
-          <CustomInput v-model="form.input" size="lg" />
-        </FormItem>
-        <FormItem label="Use delete btn">
-          <CustomInput v-model="form.input" use-delete />
-        </FormItem>
-        <FormItem label="Disabled">
-          <CustomInput v-model="form.input" disabled />
+        <FormItem label="Filled">
+          <CustomInput v-model="form.input" placeholder="내용을 입력하세요." />
         </FormItem>
         <FormItem label="Readonly">
           <CustomInput v-model="form.input" readonly />
         </FormItem>
-        <FormItem label="Error">
-          <CustomInput v-model="form.input" valid-message="입력하신 내용을 확인해주세요." />
+        <FormItem label="Disabled">
+          <CustomInput v-model="form.input" disabled />
         </FormItem>
       </form>
     </div>
     <hr>
+    <div class="my-10">
+      <mark class="inline-block text-xl font-bold" style="background:#F0E4FF">
+        Use Icon
+      </mark>
+      <form class="form content-box">
+        <FormItem label="Default">
+          <CustomInput v-model="noData" useBtn placeholder="내용을 입력하세요." />
+        </FormItem>
+        <FormItem label="Filled">
+          <CustomInput v-model="form.input" useBtn placeholder="내용을 입력하세요." />
+        </FormItem>
+        <FormItem label="Readonly">
+          <CustomInput v-model="form.input" useBtn readonly />
+        </FormItem>
+        <FormItem label="Disabled">
+          <CustomInput v-model="form.input" useBtn disabled />
+        </FormItem>
+      </form>
+    </div>
   </div>
 </template>
 
